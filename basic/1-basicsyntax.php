32f3dd7a3db19167cc3e07aa4b78ef925a8cfe1a
Raphael Torres <contato.raphaeltorres@gmail.com>
2017-04-17 14:27:40 -0300

    Update 1-basicsyntax.php

--- basic/1-basicsyntax.php
+++ basic/1-basicsyntax.php
@@ -4,7 +4,8 @@
  -One line comment: # or // - Example: //Comment or #Comment
  -Multiple lines comment: /**/ - Example: /* line 1 line 2 line3 etc */
 
-•Variables:
+
+•Variables Syntax:
  -Syntax: $name = <values>;
  -Notes:
    -all variables start with $(dollar sign)
@@ -12,10 +13,35 @@
    -the name must be in camel case(in case it have multiple words): Example: nameInCamelCase
    -name cannot start with numbers, only with a letter or undescore(_): Example: Wrong($1var) Right($_Var1)
    -name can only contain alpha-numeric characters and underscores(A-z, 0-9, and _ ): Example: Right($Name_1) Wrong($Name&1)
- 
-
-
-
-
+  
+ •Variables Attributes:
+  -Data types in PHP:
+   -Example:
+    $wholeNumber = 10; // int
+    $decimalNumber = 0.5; // double, float or decimal
+    $myCharacter = "p"; // character
+    $myString = "Hello World!"; // string
+    $myBoolean = true; // boolean
+  -Php is a weakly typed language.That means you don't need to declare the variable type, neither need to cast
+   them, because PHP do those things automatically.
+   -Example:
+    $var1 = 10; //You've set the int var1 value as 10
+    $var1 = "TI Expert"; // Now, var1 is a string with "TI Expert" as it's value
+    $var1 = false; // Now, var1 is boolean and it's value is true
+  
+  
+  •Output command in PHP:
+   -We use the echo command in PHP to output strings to the user
+    -Example:
+     echo "Hello World!!!"; // Output: Hello World!!!
+   -We can use echo to output arithmetic operations too:
+    -Example:
+     echo 7 * 5; //Output: 35
+  
+  •Concatenation:
+   -To join multiple strings, we use the concatenation operator (.).Note that other languages usually use the
+    (+) as concatenation operator.
+    -Example:
+     echo "Hello " . "World!!!"; //Output: Hello World!!!
 
 */
